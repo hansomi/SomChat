b2493bc2d76ce0acf2afad2528959bf3322cec84
hansomi <ijanamilot@gmail.com>
2018-10-06 23:11:10 +0900

    First Commit

--- app/src/main/java/com/example/lucete/somchat/ChatMessage.java
+++ app/src/main/java/com/example/lucete/somchat/ChatMessage.java
@@ -1,4 +1,71 @@
 package com.example.lucete.somchat;
 
 public class ChatMessage {
+
+    private String id;
+    private String text;
+    private String name;
+    private String photoUrl;
+    private String imageUrl;
+    private long time;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String text, String name, String photoUrl, String imageUrl, long time) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.imageUrl = imageUrl;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+
+    }
 }
--- app/src/main/java/com/example/lucete/somchat/MyUtils.java
+++ app/src/main/java/com/example/lucete/somchat/MyUtils.java
@@ -1,4 +1,16 @@
 package com.example.lucete.somchat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class MyUtils {
+
+    public static String convertTime(long timestamp) {
+        SimpleDateFormat sdf;
+        sdf = new SimpleDateFormat("HH:mm");
+        Date date = new Date(timestamp);
+        sdf.setTimeZone(TimeZone.getDefault());
+        return sdf.format(date);
+    }
 }
